5c712e59f652ef4d6b8991eb74531c93843f6836
Alexander Popov <admin@oceandb.ru>
2021-11-08 17:24:46 +0300

    Initial C

---
+++ change.js
@@ -0,0 +1,21 @@
+<!--
+ret=new Image(); ret.src="img/ret_a.gif";
+cons=new Image(); cons.src="img/cons_a.gif";
+next=new Image(); next.src="img/next_a.gif";
+ret2=new Image(); ret2.src="img/ret.gif";
+cons2=new Image(); cons2.src="img/cons.gif";
+next2=new Image(); next2.src="img/next.gif";
+
+function up(name)
+{
+if (name=="ret") {document.ret.src=eval(name+".src")}
+if (name=="cons") {document.cons.src=eval(name+".src")}
+if (name=="next") {document.next.src=eval(name+".src")}
+}
+function down(name)
+{
+if (name=="ret2") {document.ret.src=eval(name+".src")}
+if (name=="cons2") {document.cons.src=eval(name+".src")}
+if (name=="next2") {document.next.src=eval(name+".src")}
+}
+//-->
